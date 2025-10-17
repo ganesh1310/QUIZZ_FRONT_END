2513ebeaf065256f3b48a3e85d9f47c989f3ac7d
unknown <gavhaneganesh1313@gmail.com>
2025-10-17 13:56:19 +0530

    Directives Added

--- src/app/Components/dashboard-component/user-pannel/user-pannel.ts
+++ src/app/Components/dashboard-component/user-pannel/user-pannel.ts
@@ -9,6 +9,11 @@ import { Component, Input, OnChanges } from '@angular/core';
 })
 export class UserPannel implements OnChanges {
   @Input() role: any;
+  users : any = [
+    { name: 'Alice', role: 'admin' , isAdmin : true},
+    { name: 'Bob', role: 'user' , isAdmin : false},
+    { name: 'Charlie', role: 'moderator' , isAdmin : false}
+  ]
 
   ngOnChanges(simpleChanges: any): void {
     console.log('User Panel Changes:', simpleChanges);
